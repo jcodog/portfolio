59896b14f3fe58d85de351653abc49df1903d4ab
JCoDog <jcodog@jconet.co.uk>
2026-07-23 03:08:04 -0700

    refactor: compose home header from feature content

--- src/features/home/shell.tsx
+++ src/features/home/shell.tsx
@@ -1,11 +1,24 @@
-import { SiteHeader } from "@/components/layout/site-header"
+import { IconBrandGithub } from "@tabler/icons-react"
 
+import { SiteHeader } from "@/components/layout/site-header"
+import { homeContent } from "@/features/home/config/content"
 import { Hero } from "@/features/home/hero/hero"
 
 export const HomeShell = () => {
+  const { header } = homeContent
+
   return (
     <>
-      <SiteHeader />
+      <SiteHeader
+        brand={header.brand}
+        navigation={header.navigation}
+        actions={[
+          {
+            ...header.github,
+            icon: <IconBrandGithub aria-hidden="true" />,
+          },
+        ]}
+      />
 
       <main>
         <Hero />
